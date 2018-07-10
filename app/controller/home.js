087067693048ab422d3790b4daa861ee4418e32b
yananlandd01 <yananlandd01@hk01.com>
2018-07-10 20:49:35 +0800

    add query password from table of username

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,10 +1,35 @@
 'use strict';
-
 const Controller = require ('egg').Controller;
+
+// assembly response body
+const assemblyBody = (code, msg, data) => {
+  const body = {
+    code: code,
+    msg: msg,
+    data: data,
+  };
+  return body;
+};
+
 class HomeController extends Controller {
   async index () {
-    console.log (this.ctx);
-    this.ctx.body = 'hi, egg';
+    // 'this.ctx.query' can get query of requests
+    const querySearch = this.ctx.query.userName;
+    const password = this.ctx.query.password;
+
+    // get db from the table of username with async
+    const queryData = await this.ctx.app.mysql.get ('username', {
+      username: querySearch,
+    });
+
+    if (parseInt (password) === queryData.password) {
+      body = assemblyBody (0, 'success', {
+        auth: {},
+      });
+    } else {
+      body = assemblyBody (-1, 'failed', {});
+    }
+    this.ctx.body = JSON.stringify (body);
   }
 }
 
--- app/router.js
+++ app/router.js
@@ -4,7 +4,6 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = async app => {
-  const client1 = await app.mysql.get ('username');
   const {router, controller} = app;
   router.get ('/login', controller.home.index);
   router.get ('/', controller.home.index);
